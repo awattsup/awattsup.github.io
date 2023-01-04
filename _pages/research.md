88f878f2f348d6eea3c8e15d03b503519e8bb787
awattsup <adamwatts1994@gmail.com>
2023-01-04 15:21:55 +0800

    many updates

---
+++ _pages/research.md
@@ -0,0 +1,47 @@
+---
+layout: collection
+title: "Research"
+collection: "research"
+permalink: "/research/"
+entries_layout: grid
+classes: wide
+
+header:
+  overlay_color: "#000"
+  overlay_filter: "0.2"
+  overlay_image: "/assets/images/misc/andromedawan.jpeg"
+
+
+collaboration_links:
+  - image_path: "/assets/images/mauve_logo_noname.png"
+    url: https://mauve.icrar.org/index.html
+  - image_path: "/assets/images/geckos_logo.png"
+    # url:  
+  - image_path: "/assets/images/vertico_logo-spiral_grey.png"
+    url: https://www.verticosurvey.com/home
+---
+
+### My research focus is understanding how the gas within galaxies is impacted by the evolution of galaxies themselves. 
+{: style="text-align: left;"}
+
+These gas reservoirs participate in a complex cycle of consumption to form new stars, replenishment from pristine gas inflows, and heavy element-enriched feedback from dying stars. However, galaxies are rarely able to recover a reservoir of star-forming gas once it it is gone. 
+{: style="text-align: left;"}
+
+_Understanding how, and why, galaxies lose their star-forming gas is essential to understanding galaxy evolution._
+{: style="text-align: center;"}
+
+
+#### Current research
+ I am currently working on several research projects, including:
+  * How gas removal impacts the atomic and molecular gas of galaxies in clusters
+  * The properties stellar feedback-driven outflows in galaxies and their impact on their atomic and molecular gas
+  * Connecting disturbances between the different gas phases of galaxies
+ 
+ as a part of these great collarborations:
+{% include gallery id="collaboration_links" %}
+
+
+#### My substantial research items are listed here with the most recent at the top
+
+
+
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "PhD research",
+        "excerpt":"My PhD thesis was completed at the University of Western Australia under the supervision of Associate Professor Barbara Catinella and Professors Luca Cortese and Chris Power. I used some of the most sensitive observations of cold gas in nearby galaxies from the xGASS survey, cosmological numerical simulations, and optical integral...","categories": [],
+        "tags": [],
+        "url": "/research/2023-01-03-phd_research/",
+        "teaser": "/assets/images/misc/spec_examples_asym.png"
+      },{
+    "title": "Page Not Found",
+    "excerpt":"Sorry, but the page you were trying to view does not exist.  ","url": "http://localhost:4000/404.html"
+  },{
+    "title": "Home",
+    "excerpt":" ","url": "http://localhost:4000/"
+  },{
+    "title": " ",
+    "excerpt":"                                                                                                                                                 I am a postdoctoral sesearch associate at the International Centre for Radio Astronomy Research, located at The University of Western Australia.     My research is focused on understanding how the evolution of galaxies impacts their gas content - the fuel for forming new stars.                                                                                                                                                                                        Recent news                                                                                                                                                                             Latest research                                                                                                                                                                             Under construction                                                                 ","url": "http://localhost:4000/"
+  },{
+    "title": null,
+    "excerpt":"var idx = lunr(function () { this.field('title') this.field('excerpt') this.field('categories') this.field('tags') this.ref('id') this.pipeline.remove(lunr.trimmer) for (var item in store) { this.add({ title: store[item].title, excerpt: store[item].excerpt, categories: store[item].categories, tags: store[item].tags, id: item }) } }); $(document).ready(function() { $('input#search').on('keyup', function () { var resultdiv = $('#results'); var query = $(this).val().toLowerCase(); var result = idx.query(function...","url": "http://localhost:4000/assets/js/lunr/lunr-en.js"
+  },{
+    "title": null,
+    "excerpt":"step1list = new Array(); step1list[\"ΦΑΓΙΑ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΟΥ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΩΝ\"] = \"ΦΑ\"; step1list[\"ΣΚΑΓΙΑ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΟΥ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΩΝ\"] = \"ΣΚΑ\"; step1list[\"ΟΛΟΓΙΟΥ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΑ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΩΝ\"] = \"ΟΛΟ\"; step1list[\"ΣΟΓΙΟΥ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΑ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΩΝ\"] = \"ΣΟ\"; step1list[\"ΤΑΤΟΓΙΑ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΟΥ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΩΝ\"] = \"ΤΑΤΟ\"; step1list[\"ΚΡΕΑΣ\"]...","url": "http://localhost:4000/assets/js/lunr/lunr-gr.js"
+  },{
+    "title": null,
+    "excerpt":"var store = [ {%- for c in site.collections -%} {%- if forloop.last -%} {%- assign l = true -%} {%- endif -%} {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%} {%- for doc in docs -%} {%- if doc.header.teaser -%} {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture...","url": "http://localhost:4000/assets/js/lunr/lunr-store.js"
+  },{
+    "title": "News",
+    "excerpt":"{% include feature_row id=\"MAUVE1\" type='left' %}  {% include feature_row id=\"MAUVE1\" type='right' %}        ","url": "http://localhost:4000/news/"
+  },{
+    "title": "Research",
+    "excerpt":"### My research focus is understanding how the gas within galaxies is impacted by the evolution of galaxies themselves. {: style=\"text-align: left;\"} These gas reservoirs participate in a complex cycle of consumption to form new stars, replenishment from pristine gas inflows, and heavy element-enriched feedback from dying stars. However, galaxies...","url": "http://localhost:4000/research/"
+  },{
+    "title": "Search",
+    "excerpt":"","url": "http://localhost:4000/search/"
+  },{
+    "title": null,
+    "excerpt":"{% if page.xsl %} {% endif %} {% assign collections = site.collections | where_exp:'collection','collection.output != false' %}{% for collection in collections %}{% assign docs = collection.docs | where_exp:'doc','doc.sitemap != false' %}{% for doc in docs %} {{ doc.url | replace:'/index.html','/' | absolute_url | xml_escape }} {% if doc.last_modified_at or doc.date...","url": "http://localhost:4000/sitemap.xml"
+  },{
+    "title": null,
+    "excerpt":"Sitemap: {{ \"sitemap.xml\" | absolute_url }} ","url": "http://localhost:4000/robots.txt"
+  },{
+    "title": null,
+    "excerpt":"{% if page.xsl %}{% endif %}Jekyll{{ site.time | date_to_xmlschema }}{{ page.url | absolute_url | xml_escape }}{% assign title = site.title | default: site.name %}{% if page.collection != \"posts\" %}{% assign collection = page.collection | capitalize %}{% assign title = title | append: \" | \" | append: collection %}{% endif...","url": "http://localhost:4000/feed.xml"
+  }]
